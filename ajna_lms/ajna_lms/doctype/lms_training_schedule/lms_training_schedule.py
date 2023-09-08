244fc2b9fa78aef9a98d047bff3bfa1e81046823
Paramesh Nalla <paramesh@ajna.cloud>
2023-09-08 07:23:15 +0000

    Initial Commit

---
+++ ajna_lms/ajna_lms/doctype/lms_training_schedule/lms_training_schedule.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Vivek and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
+
+class LMSTrainingSchedule(Document):
+	pass
